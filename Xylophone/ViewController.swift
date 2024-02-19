1e20c1cb62e0efb290e13d3f9c0f0f08de983c76
kollist <alaouikhalid2001@gmail.com>
2024-02-19 15:14:32 +0100

    creating an app so where you click you hear a sound and it looks like a piano

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(name: sender.currentTitle!)
+    }
+    func playSound(name: String) {
+        
+        let path = Bundle.main.url(forResource: name, withExtension:"wav")
+        player = try! AVAudioPlayer(contentsOf: path!)
+        player?.play()
+
+    }
+}
+
